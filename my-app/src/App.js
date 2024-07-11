24b23bbd251a8ef3b0372f90a9a8c7e7472803fd
Mares Popa <a.popa@goit.global>
2024-07-11 19:47:48 +0300

    Add another change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a new change.
+          This is another new change. 2024
         </p>
         <a
           className="App-link"
